e257552b10d117337cf27d97e6d10199fa07bc71
DarkAce65 <tvdood99@gmail.com>
2017-01-21 18:43:01 -0500

    Fix playlist

--- imports/ui/pages/playlist.js
+++ imports/ui/pages/playlist.js
@@ -1,9 +1,14 @@
 import { Template } from "meteor/templating";
+import { FlowRouter } from "meteor/kadira:flow-router";
 
 import "./playlist.html";
 
 Template.body.helpers({
-	"playlist": function() {
-		return {};
+	"items": function() {
+		var list = Lists.findOne(FlowRouter.getParam("id"));
+		if(list) {
+			return list.items;
+		}
+		return [];
 	}
 });
