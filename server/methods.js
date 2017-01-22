0bfbedf78c82e5365fefdec7e3e9e5c025e322b1
DarkAce65 <tvdood99@gmail.com>
2017-01-21 19:57:08 -0500

    Add name and check for undefined prvt

--- server/methods.js
+++ server/methods.js
@@ -1,12 +1,16 @@
 import { Meteor } from "meteor/meteor";
 
 Meteor.methods({
-	"createPlaylist": function(prvt) {
+	"createPlaylist": function(name, prvt) {
 		if(!this.userId) {
 			throw new Meteor.Error(401, "You are not logged in.");
 		}
+		if(!prvt) {
+			prvt = false;
+		}
 		var now = new Date();
 		var list = {
+			"name": name,
 			"owner": this.userId,
 			"createdAt": now,
 			"updatedAt": now,
