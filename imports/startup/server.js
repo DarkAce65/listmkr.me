8af2ecc9ef5fc5e69b8f9d04b95847303fa9e1cb
DarkAce65 <tvdood99@gmail.com>
2017-01-21 22:28:38 -0500

    Update services format

--- imports/startup/server.js
+++ imports/startup/server.js
@@ -4,27 +4,29 @@ if(Media.find().count() === 0) {
 	var data = [
 		{
 			"type": "text",
-			"service": "",
+			"services": {},
 			"name": "Survival of the Sickest",
 			"creator": "Sharon Moalem"
 		},
 		{
 			"type": "audio",
-			"service": "spotify",
-			"url": "https://play.spotify.com/track/1sH2GeOsLFEw714Otu8P9i",
+			"services": {
+				"spotify": "https://play.spotify.com/track/1sH2GeOsLFEw714Otu8P9i"
+			},
 			"name": "Fiona Coyne",
 			"creator": "Skylar Spence",
 			"album": "Fiona Coyne/Fall Harder"
 		},
 		{
 			"type": "video",
-			"service": "youtube",
-			"url": "https://www.youtube.com/watch?v=ebILIKHi9wo",
+			"services": {
+				"youtube": "https://www.youtube.com/watch?v=ebILIKHi9wo"
+			},
 			"name": "Delta Heavy - White Flag (Official Video)",
 			"creator": "DeltaHeavyVevo"
 		}
 	];
 	data.forEach(function(doc) {
-		Media.insert(doc);
+		Media.update(doc);
 	});
 }
